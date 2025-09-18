906e17c6baebc50af7cfd847491bc458a1692b2c
Harena-Git <faniriantsoaharena@gmail.com>
2025-09-18 11:01:01 +0100

    Profil
    Little Changement

--- gestion/src/main/java/com/example/gestion/controllers/ProfilController.java
+++ gestion/src/main/java/com/example/gestion/controllers/ProfilController.java
@@ -1,15 +1,13 @@
 package com.example.gestion.controllers;
 
 import com.example.gestion.models.Profil;
-import com.example.gestion.models.Annonce;
-import com.example.gestion.repository.AnnonceRepository;
+import com.example.gestion.repository.ProfilRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-import com.example.gestion.repository.ProfilRepository;
 
 @Controller
 public class ProfilController {
@@ -25,9 +23,13 @@ public class ProfilController {
     }
 
     @GetMapping("/profil")
-    public String getProfilById(@RequestParam("idProfil") Long idProfil, Model model) {
+    public String getProfil(@RequestParam("idProfil") Integer idProfil, Model model) {
         Profil profil = profilRepository.findById(idProfil).orElse(null);
-        model.addAttribute("profil", profil);
+        if (profil != null) {
+            model.addAttribute("profil", profil);
+        } else {
+            model.addAttribute("error", "Profil non trouvé avec l'ID : " + idProfil);
+        }
         return "profil";
     }
 }
