c68ba23f1c7c1749c33b2f6c14c27021bb0f581b
Harena-Git <faniriantsoaharena@gmail.com>
2025-09-19 16:23:00 +0100

    A debuguer
    Commit fotsiny

--- gestion/src/main/java/com/example/gestion/controllers/AnnonceController.java
+++ gestion/src/main/java/com/example/gestion/controllers/AnnonceController.java
@@ -1,13 +1,18 @@
 package com.example.gestion.controllers;
 
+import java.time.LocalDate;
+
 import com.example.gestion.models.Annonce;
 import com.example.gestion.repository.AnnonceRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 @Controller
 public class AnnonceController {
@@ -18,6 +23,13 @@ public class AnnonceController {
     @GetMapping("/acceuil")
     public String getAllAnnonces(Model model) {
         List<Annonce> annonces = annonceRepository.findAll();
+        LocalDate today = LocalDate.now();
+        annonces = annonces.stream()
+                .filter(annonce -> {
+                    LocalDate dateFin = LocalDate.parse(annonce.getDate_fin());
+                    return !dateFin.isAfter(today);
+                })
+                .collect(Collectors.toList());
         model.addAttribute("annonces", annonces);
         return "Acceuil";
     }
