f654e4160b6c36db1f8fa6237225bcb8f61dea53
Harena-Git <faniriantsoaharena@gmail.com>
2025-12-14 07:06:20 +0300

    a

--- src/main/java/com/example/gestion/repository/HistoriquePosteRepository.java
+++ src/main/java/com/example/gestion/repository/HistoriquePosteRepository.java
@@ -3,6 +3,8 @@ package com.example.gestion.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.gestion.models.HistoriquePoste;
@@ -10,6 +12,6 @@ import com.example.gestion.models.Personnel;
 
 @Repository
 public interface HistoriquePosteRepository extends JpaRepository<HistoriquePoste, Integer> {
-    List<HistoriquePoste> findByPersonnelOrderByDateDebutDesc(Personnel personnel);
-    List<HistoriquePoste> findByTypeMouvement(String typeMouvement);
+    @Query("SELECT h FROM HistoriquePoste h WHERE h.personnel = :personnel ORDER BY h.date_debut DESC")
+    List<HistoriquePoste> findByPersonnelOrderByDateDebutDesc(@Param("personnel") Personnel personnel);
 }
